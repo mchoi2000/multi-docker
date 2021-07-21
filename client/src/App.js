e452ed091172c7030e5bbae2e59ac0adc9a134e6
Mihwa Choi <mihwa@us.ibm.com>
2021-07-21 14:15:13 +0900

    update the landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-        </header>
+      <div className='App'>
         <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
+          <h1>Fib Calculator</h1>
+          {/* <Link to='/'>Home</Link>
+          <Link to='/otherpage'>Other Page</Link> */}
+        </div>
+        <div>
+          <Route exact path='/' component={Fib} />
+          <Route path='/otherpage' component={OtherPage} />
         </div>
       </div>
     </Router>
